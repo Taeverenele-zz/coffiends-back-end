6a64097546a4f44d33e63622754b59d910fc03aa
Seb Brooks <sbrookstree@gmail.com>
2021-01-27 20:30:44 +1000

    updating seeds, cafe model discrepancy

--- models/cafes.js
+++ models/cafes.js
@@ -11,18 +11,6 @@ const CafeSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-<<<<<<< HEAD
-  // operating_hours: {
-  //   type: Array,
-  //   required: true,
-  // },
-  // location: {
-  //   type: Array,
-  //   required: true,
-  // },
-  // menu: { type: mongoose.Types.ObjectId, ref: "Menu" },
-  // owner: { type: mongoose.Types.ObjectId, ref: "User" },
-=======
   operating_hours: {
     type: Array,
     required: true,
@@ -31,13 +19,11 @@ const CafeSchema = new mongoose.Schema({
     type: Array,
     required: true
   },
-  // menu: { type: mongoose.Types.ObjectId, ref: "Menu" },
   menu: {
     type: Array,
     default: [objectId]
   },
   owner: { type: objectId, ref: "User" },
->>>>>>> d95131d09706220c705eae87fc721bf1a41d0f58
 });
 
 const Cafe = mongoose.model("Cafes", CafeSchema);
