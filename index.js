55cafac91881f73e8896ead2f0bfae0eeff90a86
Nele Taevere <ntaevere@gmail.com>
2021-01-27 11:19:02 +1000

    added user routes

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use("/coffees", require("./routes/coffees.js"));
 app.use("/cafes", require("./routes/cafes.js"));
+app.use("/users", require("./routes/users.js"));
 
 mongoose
   .connect(CONNECTION_URL, {
