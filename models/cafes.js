24e7824bfe9743c13d2bd05ce5d0319d01afbfd8
Nele Taevere <ntaevere@gmail.com>
2021-01-27 20:29:23 +1000

    apdated cafes model

--- models/cafes.js
+++ models/cafes.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const objectId = mongoose.Types.ObjectId
+const objectId = mongoose.Types.ObjectId;
 
 const CafeSchema = new mongoose.Schema({
   name: {
@@ -11,33 +11,16 @@ const CafeSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-<<<<<<< HEAD
-  // operating_hours: {
-  //   type: Array,
-  //   required: true,
-  // },
-  // location: {
-  //   type: Array,
-  //   required: true,
-  // },
-  // menu: { type: mongoose.Types.ObjectId, ref: "Menu" },
-  // owner: { type: mongoose.Types.ObjectId, ref: "User" },
-=======
   operating_hours: {
     type: Array,
     required: true,
   },
   location: {
     type: Array,
-    required: true
+    required: true,
   },
   // menu: { type: mongoose.Types.ObjectId, ref: "Menu" },
-  menu: {
-    type: Array,
-    default: [objectId]
-  },
-  owner: { type: objectId, ref: "User" },
->>>>>>> d95131d09706220c705eae87fc721bf1a41d0f58
+  // owner: { type: mongoose.Types.ObjectId, ref: "User" },
 });
 
 const Cafe = mongoose.model("Cafes", CafeSchema);
