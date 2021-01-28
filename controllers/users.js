d5e070231be811b0524d1c41795ef81161e599d1
Nele Taevere <ntaevere@gmail.com>
2021-01-28 14:59:52 +1000

    added find one cafe to cafes routes

--- controllers/users.js
+++ controllers/users.js
@@ -10,6 +10,16 @@ const getUsers = async (req, res) => {
   }
 };
 
+const getOneUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
 const createUser = async (req, res) => {
   const user = req.body;
   const newUser = new User(user);
@@ -22,11 +32,11 @@ const createUser = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-  const { id: _id } = req.params;
+  const { id } = req.params;
   const user = req.body;
-  if (!mongoose.Types.ObjectId.isValid(_id))
+  if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send("No cafe with that id");
-  const updatedUser = await User.findByIdAndUpdate(_id, user, { new: true });
+  const updatedUser = await User.findByIdAndUpdate(id, user, { new: true });
   res.json(updatedUser);
 };
 
@@ -35,7 +45,7 @@ const deleteUser = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send("No cafes with this id");
   await User.findByIdAndRemove(id);
-  res.json({ message: "Cafe deleted successfully" });
+  res.json({ message: "User deleted successfully" });
 };
 
-module.exports = { getUsers, createUser, deleteUser, updateUser };
+module.exports = { getUsers, getOneUser, createUser, deleteUser, updateUser };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const {
   getUsers,
+  getOneUser,
   createUser,
   deleteUser,
   updateUser,
@@ -9,6 +10,7 @@ const {
 const router = express.Router();
 
 router.get("/", getUsers);
+router.get("/:id", getOneUser);
 router.post("/", createUser);
 router.patch("/:id", updateUser);
 router.delete("/:id", deleteUser);
