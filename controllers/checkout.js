faf23bb8b35882ae733764d31b5925f4e7349947
Nele Taevere <ntaevere@gmail.com>
2021-02-05 08:04:15 +1000

    changed hard coded urls to evn variables

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -17,8 +17,8 @@ const checkout = async (req, res) => {
       },
     ],
     mode: 'payment',
-    success_url: `http://localhost:5000/orders/success/?user=${req.body.user}&cafe=${req.body.cafe}&coffee=${req.body.coffee}&size=${req.body.size}&milk=${req.body.milk}&sugar=${req.body.sugar}&time=${req.body.pickup_time}&total=${req.body.total}`,
-    cancel_url: "http://localhost:3000/"
+    success_url: `${process.env.BACK_END_URL}/orders/success/?user=${req.body.user}&cafe=${req.body.cafe}&coffee=${req.body.coffee}&size=${req.body.size}&milk=${req.body.milk}&sugar=${req.body.sugar}&time=${req.body.pickup_time}&total=${req.body.total}`,
+    cancel_url: process.env.FRONT_END_URL,
   });
 
   res.json({ id: session.id });
