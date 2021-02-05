ffa86e06e4d1429cf7c354a5a00c08c3025bc386
Seb Brooks <sbrookstree@gmail.com>
2021-02-05 13:42:34 +1000

    added return to serialize stuff

--- passport.js
+++ passport.js
@@ -2,11 +2,11 @@ const passport = require("passport");
 const User = require("./models/users.js")
 
 passport.serializeUser(function(user, done) {
-    done(null, user);
+    return done(null, user);
 });
 
 passport.deserializeUser(function(user, done) {
-    done(null, user);
+    return done(null, user);
 });
 
 passport.use(User.createStrategy());
