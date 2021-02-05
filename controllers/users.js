a93d11c6fb5a181f74f625da29ec6160b392a445
Seb Brooks <sbrookstree@gmail.com>
2021-02-05 13:54:01 +1000

    log messages for session and logout

--- controllers/users.js
+++ controllers/users.js
@@ -105,7 +105,7 @@ const changeUserPassword = (req, res) => {
 };
 
 const userSessionCheck = (req, res) => {
-  console.log("Checking if user is logged in...");
+  console.log("Checking for user session...");
   if (req.user) {
     if (req.user.role === "cafe") {
       const getCafe = Cafe.findOne({ owner: req.user._id });
@@ -134,6 +134,7 @@ const userSessionCheck = (req, res) => {
 };
 
 const logUserOut = (req, res) => {
+  console.log("Logging user out");
   req.logOut();
   res.sendStatus(200);
 };
